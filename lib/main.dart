5ae40005256d23c7868e97577f9558f96c86d3c4
marizvi <202051121@iiitvadodara.ac.in>
2021-08-31 19:59:24 +0530

    textfield added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'transaction.dart';
+import "package:intl/intl.dart";
 
 void main() { 
   runApp(MyApp());
@@ -33,7 +34,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body:Column( 
-        mainAxisAlignment:MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.start,
         children:
         <Widget>[
@@ -45,7 +45,27 @@ class MyHomePage extends StatelessWidget {
             elevation: 5,
                   ),
                 ),
-
+                Card(
+                  elevation:5,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                       children: <Widget>[
+                  TextField(decoration:InputDecoration(labelText:'Title' ) ,),
+                  TextField(decoration: InputDecoration(labelText:'Amount'),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue[600])
+                    ),
+                  child: Text('Add Transaction'),
+                  onPressed: (){},
+                  )
+                ],
+                ),)
+                ),
                 //cannot use below approach because we do not know beforehand 
                 //how much widgets do we need(i.e how much cards do we need)
           // Column(children: <Widget>[
@@ -54,20 +74,22 @@ class MyHomePage extends StatelessWidget {
           // ])
 
           Column(
-            children: transactions.map((obj){
+            // here we have not used this [] brackets because transactions is itself a list of Transaction
+            //and cards cannot be defined under the list,
+            children: 
+            transactions.map((obj){
               return Card(
-
                 //row is used to seperate amount section from actual content section...
                 child:Row(children:
-                 <Widget>[
+                 [
                           //row 1
                           Container(
                             decoration: BoxDecoration(border:
                                Border.all(color:Colors.purple,width: 2,),borderRadius: BorderRadius.circular(10),) ,
                             margin: EdgeInsets.symmetric(vertical: 10,horizontal: 15),
                             padding: EdgeInsets.all(10),
-                            child:Text(
-                              obj.amount.toString(),
+                            child:Text('\u{20B9} '+
+                            obj.amount.toString(),
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 15,
@@ -76,10 +98,13 @@ class MyHomePage extends StatelessWidget {
                               )
                                   ),
                           //row 2
-                          Column(children: <Widget>[
-                            Text(obj.title),
-                            Text(obj.date.toString()),
-                          ],)        
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                            Text(obj.title, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                            Text(
+                              DateFormat.yMMMEd().format(obj.date),style: TextStyle(color: Colors.grey),),
+                          ],),        
                   
                 ],)
                 );
