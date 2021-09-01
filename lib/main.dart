29e01977637e50e63f99bc863f6ff38d9b46c74e
marizvi <202051121@iiitvadodara.ac.in>
2021-09-01 07:28:23 +0530

    controller added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'models/transaction.dart';
-import "package:intl/intl.dart";
+import 'widgets/transaction_list.dart';
 
 void main() { 
   runApp(MyApp());
@@ -17,14 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      't1','New Shoes' ,6999 , DateTime.now(),
-      ), 
-    Transaction(
-      't2','weekly Groceries' ,2500 , DateTime.now(),
-      ), 
-  ];
+  
 // String titleInput="";
 // String amountInput="";
 
@@ -92,45 +84,7 @@ final amountController = TextEditingController();
           //   Card(),
           //   Card(),
           // ])
-
-          Column(
-            // here we have not used this [] brackets because transactions is itself a list of Transaction
-            //and cards cannot be defined under the list,
-            children: 
-            transactions.map((obj){
-              return Card(
-                //row is used to seperate amount section from actual content section...
-                child:Row(children:
-                 [
-                          //row 1
-                          Container(
-                            decoration: BoxDecoration(border:
-                               Border.all(color:Colors.purple,width: 2,),borderRadius: BorderRadius.circular(10),) ,
-                            margin: EdgeInsets.symmetric(vertical: 10,horizontal: 15),
-                            padding: EdgeInsets.all(10),
-                            child:Text('\u{20B9} '+
-                            obj.amount.toString(),
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Colors.purple,
-                              ),
-                              )
-                                  ),
-                          //row 2
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                            Text(obj.title, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                            Text(
-                              DateFormat.yMMMEd().format(obj.date),style: TextStyle(color: Colors.grey),),
-                          ],),        
-                  
-                ],)
-                );
-            }
-          ).toList(),),
-
+            TransactionList() 
         ]
       )
     );
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import "package:intl/intl.dart";
+
+class TransactionList extends StatefulWidget {
+
+  @override
+  _TransactionListState createState() => _TransactionListState();
+}
+
+class _TransactionListState extends State<TransactionList> {
+  final List<Transaction> _usertransaction = [
+     Transaction(
+      't1','New Shoes' ,6999 , DateTime.now(),
+      ), 
+    Transaction(
+      't2','weekly Groceries' ,2500 , DateTime.now(),
+      ), 
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+            // here we have not used this [] brackets because transactions is itself a list of Transaction
+            //and cards cannot be defined under the list,
+            children: 
+            _usertransaction.map((obj){
+              return Card(
+                //row is used to seperate amount section from actual content section...
+                child:Row(children:
+                 [
+                          //row 1
+                          Container(
+                            decoration: BoxDecoration(border:
+                               Border.all(color:Colors.purple,width: 2,),borderRadius: BorderRadius.circular(10),) ,
+                            margin: EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+                            padding: EdgeInsets.all(10),
+                            child:Text('\u{20B9} '+
+                            obj.amount.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.purple,
+                              ),
+                              )
+                                  ),
+                          //row 2
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                            Text(obj.title, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                            Text(
+                              DateFormat.yMMMEd().format(obj.date),style: TextStyle(color: Colors.grey),),
+                          ],),        
+                  
+                ],)
+                );
+            }
+          ).toList(),);
+  }
+}
