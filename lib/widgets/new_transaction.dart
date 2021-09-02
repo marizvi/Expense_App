e16986216ce3e8be132e43e24fb586e5949c3c9a
marizvi <202051121@iiitvadodara.ac.in>
2021-09-02 17:01:05 +0530

    number keyboard

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -29,6 +29,7 @@ class NewTransaction extends StatelessWidget {
                   TextField(
                     decoration: InputDecoration(labelText:'Amount'),
                     controller: amountController,
+                    keyboardType: TextInputType.number
                     // onChanged: (val){
                     //   amountInput=val;
                     // },
