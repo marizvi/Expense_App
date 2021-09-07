f891a81b68cc4e0ec57572f1bf67287d6e79e324
marizvi <202051121@iiitvadodara.ac.in>
2021-09-07 12:56:34 +0530

    chart progress

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+
+      List<Map<String,Object>> get groupedTransactionValues{
+        return List.generate(7,(index){
+          return {
+            'Day' : 'T',
+            'amount' : 9.99,
+          };
+        });
+      }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
+      child: Row(children: [
+        
+      ],),
     );
   }
 }
